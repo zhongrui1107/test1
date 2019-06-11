2526cc8ede90a614f1320e2634d7e9131ce050c5
zhongrui1107 <51185805+zhongrui1107@users.noreply.github.com>
2019-06-11 16:26:30 +0800

    Update Test1.java

--- test3/src/test3/Test1.java
+++ test3/src/test3/Test1.java
@@ -4,6 +4,7 @@ public class Test1 {
 	private void SYso() {
 		System.out.println("123125456456");
 		System.out.println("12312222236");
+                System.out.println("12skkdkss236");
 
 	}
 	public static void main(String[] args) {
